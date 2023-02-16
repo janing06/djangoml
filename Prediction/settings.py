311617a4c1f731cfde277ea9c82ee4a366246ff8
janing06 <120067518+janing06@users.noreply.github.com>
2023-02-16 09:31:22 +0800

    asdasd

--- Prediction/settings.py
+++ Prediction/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "Prediction.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'tempates')],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
